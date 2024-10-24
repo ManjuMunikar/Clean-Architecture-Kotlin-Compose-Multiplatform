3a6b64dbcfc21c50152c3ff4d1142dec6302ea52
Manju Munikar <manjum@vianet.com.np>
2024-10-24 15:17:56 +0545

    splash screen in android

--- composeApp/src/androidMain/kotlin/hrm/compose/multiplatform/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/hrm/compose/multiplatform/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
 
         setContent {
             App( batteryManager = remember { BatteryManager(applicationContext) })
